d68906e39f2f81694e044a7325e4e59908997fea
TomBurch <thomas.burch1306@gmail.com>
2021-06-21 01:21:18 +0100

    Update PageController.php

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -24,7 +24,7 @@ class PageController extends Controller
             return url('images/'.$file);
         });
 
-		return view('home.index', compact('banners'));
+        return view('home.index', compact('banners'));
 	}
 
     public function roster()
